48f218daaf52602d75c9846081fa46aaf6cd9c21
Christian Hotte <christianwhotte@gmail.com>
2022-09-07 14:41:01 -0400

    Most RatBoid Test Features Finalized
    -RatBoids now follow a trail, allowing for finer control of ratpack
    -Acceleration and drag based on target adjacency allow ratpack to cling to target much better (works better with a smaller pack)
    -Settings have been adjusted for much higher rat speeds in order to increase responsiveness

--- Assets/Scripts/Testing/BoidManager.cs
+++ Assets/Scripts/Testing/BoidManager.cs
@@ -10,31 +10,103 @@ public class BoidManager : MonoBehaviour
     [SerializeField, Tooltip("Object which spawned rats will seek toward")] private Transform targetRat;
 
     //Settings:
-    [Header("Settings:")]
+    [Header("General Settings:")]
     [SerializeField, Tooltip("Height at which new rats will be spawned")]                           private float spawnHeight;
     [SerializeField, Tooltip("Random distance range by which newly-spawned rats will be offset")]   private float spawnPositionRandomness;
     [SerializeField, Min(0), Tooltip("Maximum speed at which rats can travel")]                     private float maxSpeed;
     [SerializeField, Min(0), Tooltip("Separation distance under which rats can affect each other")] private float neighborRadius;
     [SerializeField, Min(0), Tooltip("Target separation distance between rats")]                    private float separation;
-    [Space()]
-    [SerializeField, Min(0), Tooltip("Range at which adjacency to target will begin to slow rats down")] private float targetDragRange;
-    [SerializeField, Tooltip("Curve describing drag depending on how close rats are to target")]         private AnimationCurve targetDragCurve;
+    [Header("Target Settings:")]
+    [SerializeField, Min(0), Tooltip("Number of units by which trail length is reduced every second")]               private float trailFadeSpeed;
+    [SerializeField, Min(0), Tooltip("Maximum length (in units) of target trail")]                                   private float maxTrailLength;
+    [SerializeField, Min(0), Tooltip("Range at which adjacency to target will begin to slow rats down")]             private float targetDragRange;
+    [SerializeField, Tooltip("Curve describing drag depending on how close rats are to target")]                     private AnimationCurve targetDragCurve;
+    [SerializeField, Tooltip("Curve describing trail-induced velocity falloff over trail length")]                   private AnimationCurve trailFalloffCurve;
+    [SerializeField, Min(0), Tooltip("Distance from target at which rats will begin to have a higher max speed")]    float targetSpeedIncreaseRadius;
+    [SerializeField, Tooltip("Curve describing additive multiplier for max speed depending on adjacency to target")] private AnimationCurve targetAddSpeedCurve;
     [Header("Rule Weights:")]
     [SerializeField, Tooltip("Influence clumping rule has on rat behavior")]           private float clumpWeight = 1;
     [SerializeField, Tooltip("Influence separation rule has on rat behavior")]         private float separationWeight = 1;
     [SerializeField, Tooltip("Influence conformance rule has on rat behavior")]        private float conformWeight = 1;
     [SerializeField, Tooltip("Influence target rule has on rat behavior")]             private float targetWeight = 1;
     [SerializeField, Tooltip("Influence target drag rule has on rat behavior")]        private float targetDragWeight = 1;
-    [SerializeField, Tooltip("Influence camera confinement rule has on rat behavior")] private float cameraConfineWeight = 1;
 
     //Runtime vars:
-    private List<Transform> rats = new List<Transform>(); //List of all spawned ratboids in scene
+    private List<Transform> rats = new List<Transform>();    //List of all spawned ratboids in scene
+    private List<Vector2> targetTrail = new List<Vector2>(); //List of previous positions of target stored in memory
+    private Vector2 currentTargetPos;                        //Target position of targetRat (updated by input)
+    private float targetRatHeight;                           //Set height of targetRat
 
     //RUNTIME METHODS:
+    private void Start()
+    {
+        //Get initial variables:
+        targetRatHeight = spawnHeight * targetRat.localScale.x; //Determine height of targetRat based on set spawn height and target scale
+        targetTrail.Add(Vector2.zero);                          //Start target trail list with an initial position at world zero
+    }
     private void FixedUpdate()
     {
+        //Update target position:
+        if (targetRat != null) UpdateTarget(Time.fixedDeltaTime); //Update position of target rat
+
         //Move ratboids:
-        if (rats.Count > 0) SimulateRatMovement(Time.deltaTime); //Move ratboids if there is at least one ratboid in scene
+        if (rats.Count > 0) SimulateRatMovement(Time.fixedDeltaTime); //Move ratboids if there is at least one ratboid in scene
+    }
+    private void UpdateTarget(float deltaTime)
+    {
+        //Check for change in position:
+        Vector2 targetPos = new Vector2(targetRat.position.x, targetRat.position.z); //Get actual position of target as 2D vector
+        float posDiff = Vector2.Distance(currentTargetPos, targetPos);               //Get linear distance target has traveled since last check
+        if (posDiff > 0) //Target has moved since last check
+        {
+            targetTrail.Insert(0, currentTargetPos);                                                     //Add new target position to beginning trail list (ensuring that no two entries occupy the same point)
+            Vector3 newTargetPos = new Vector3(currentTargetPos.x, targetRatHeight, currentTargetPos.y); //Get 3D vector for new targetRat position
+            targetRat.position = newTargetPos;                                                           //Move targetRat object to designated position
+        }
+
+        //Decrease trail length:
+        if (targetTrail.Count > 1) //TargetTrail length is still greater than zero
+        {
+            //Get total length:
+            float totalLength = 0; //Initialize container for storing total trail length
+            for (int i = 1; i < targetTrail.Count; i++) //Iterate through each item in target trail (excluding first entry)
+            {
+                totalLength += Vector2.Distance(targetTrail[i - 1], targetTrail[i]); //Add distance between current and previous points to total trail length value
+            }
+
+            //Determine reduction amount:
+            float trimLength = trailFadeSpeed * deltaTime;                           //Get base (constant) length reduction amount
+            trimLength += Mathf.Max(0, (totalLength - trimLength) - maxTrailLength); //If (after pruning) the trail will exceed max length, add all excess length to trim amount
+
+            //Trim trail:
+            for (int i = targetTrail.Count - 1; i > 0; i--) //Iterate backwards through trail, excluding first element
+            {
+                float segmentLength = Vector2.Distance(targetTrail[i], targetTrail[i - 1]); //Get length of current segment
+                if (segmentLength > trimLength) //Segment is longer than remaining distance which needs to be pruned
+                {
+                    targetTrail[i] = Vector2.MoveTowards(targetTrail[i], targetTrail[i - 1], trimLength); //Reduce length of trail end by remaining prune length
+                    break;                                                                                //Break from loop
+                }
+                else //Segment will be completely removed by pruning operation
+                {
+                    targetTrail.RemoveAt(targetTrail.Count - 1); //Remove segment from trail
+                    trimLength -= segmentLength;                 //Subtract segment length from prune length (since entire segment has been pruned)
+                    if (trimLength == 0) break;                  //Special case for loop break (in case prune length was exactly equal to segment length)
+                }
+            }
+        }
+
+        //Visualize trail:
+        if (targetTrail.Count > 1) //TargetTrail contains multiple elements
+        {
+            Vector3 prevPoint = new Vector3(targetTrail[0].x, targetRatHeight, targetTrail[0].y); //Initialize container for storing adjacent points and set value to that of first point in trail
+            for (int i = 1; i < targetTrail.Count; i++) //Iterate through each item in target trail (excluding first entry)
+            {
+                Vector3 point = new Vector3(targetTrail[i].x, targetRatHeight, targetTrail[i].y); //Get world position of current point in trail
+                Debug.DrawLine(prevPoint, point, Color.green);                                    //Draw line between each point pair
+                prevPoint = point;                                                                //Save calculated point for use by next item
+            }
+        }
     }
     private void SimulateRatMovement(float deltaTime)
     {
@@ -81,12 +153,22 @@ public class BoidManager : MonoBehaviour
                 conformVel /= 8;
             }
 
-            //Do target rule:
-            Vector3 ratPosDiff = targetRat.transform.position - rat.transform.position;
-            Vector2 targetVel = new Vector2(ratPosDiff.x, ratPosDiff.z); //Get direction of velocity moving rats toward target
-
-            //Do camera confinement rule:
-
+            //Do target rule (with trail):
+            Vector2 targetVel = targetTrail[0] - pos;
+            if (targetTrail.Count > 1) //There is more than one item in target trail (for rats to home toward)
+            {
+                for (int i = 1; i < targetTrail.Count; i++) //Iterate through each point in target trail, skipping first item
+                {
+                    float forceModifier = trailFalloffCurve.Evaluate(1 - (i / targetTrail.Count)); //Evaluate curve to get modifier for trail force falloff
+                    targetVel += (targetTrail[i] - pos) * forceModifier;                           //Add additional velocity for each item in trail
+                }
+            }
+            float actualTargetDistance = Vector2.Distance(pos, new Vector2(targetRat.position.x, targetRat.position.z)); //Get distance between rat and actual target position
+            float currentMaxSpeed = maxSpeed;
+            if (actualTargetDistance < targetSpeedIncreaseRadius) //Rat is close enough to target for a speed boost
+            {
+                targetVel *= 1 + targetAddSpeedCurve.Evaluate(Mathf.InverseLerp(targetSpeedIncreaseRadius, 0, actualTargetDistance)); //Add speed boost depending on distance from target
+            }
 
             //Apply rules:
             newVel += clumpingVel * clumpWeight;
@@ -95,26 +177,22 @@ public class BoidManager : MonoBehaviour
             newVel += targetVel * targetWeight;
 
             //Do target drag rule (late):
-            float targetDistance = ratPosDiff.magnitude; //Get distance between target and rat
             Vector2 targetDragVel = Vector2.zero;
-            if (targetDistance <= targetDragRange) //Rat is close enough to target to induce drag
+            if (actualTargetDistance <= targetDragRange) //Rat is close enough to target to induce drag
             {
-                float dragInterpolant = Mathf.InverseLerp(targetDragRange, 0, targetDistance);
-                targetDragVel = -newVel; //Initialize drag as reverse velocity of rat
+                float dragInterpolant = Mathf.InverseLerp(targetDragRange, 0, actualTargetDistance);
+                targetDragVel = -newVel;                                    //Initialize drag as reverse velocity of rat
                 targetDragVel *= targetDragCurve.Evaluate(dragInterpolant); //Apply distance-sensitive intensity curve to drag force
             }
             newVel += targetDragVel * targetDragWeight;
 
             //Apply velocity:
-            if (newVel.magnitude > maxSpeed) newVel = newVel.normalized * maxSpeed; //Clamp velocity
+            if (newVel.magnitude > currentMaxSpeed) newVel = newVel.normalized * currentMaxSpeed; //Clamp velocity
             rat.GetComponent<TestBoidController>().velocity = newVel;
             newVel *= deltaTime; //Temporarily apply deltaTime to velocity
             rat.position = new Vector3(rat.position.x + newVel.x, spawnHeight, rat.position.z + newVel.y);
         }
     }
-
-    //RAT RULES:
-
 
     //INPUT METHODS:
     public void OnSpawnRat(InputAction.CallbackContext context)
@@ -135,9 +213,7 @@ public class BoidManager : MonoBehaviour
         Ray mouseRay = Camera.main.ScreenPointToRay(value); //Get ray from camera based on pointer position
         if (Physics.Raycast(mouseRay, out RaycastHit hit)) //See if ray is hitting floor plane
         {
-            Vector3 targetPoint = hit.point;                      //Get point on plane to move target to
-            targetPoint.y = spawnHeight * targetRat.localScale.x; //Use rat scale and spawn height to find static ground offset for target
-            targetRat.position = targetPoint;                     //Move target to computed point
+            currentTargetPos = new Vector2(hit.point.x, hit.point.z); //Update current target position
         }
     }
     public void OnScrollSpawn(InputAction.CallbackContext context)
