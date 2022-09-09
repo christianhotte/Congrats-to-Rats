c79362c74dfbfe6e3cd0b35e4ac674596e5de06e
Christian Hotte <christianwhotte@gmail.com>
2022-09-09 17:03:42 -0400

    Rat Trail Implemented
    -Trail length is now limited to exact unit value
    -Trail segment length is set to a strict minimum, allowing exact complexity (and performance) of trail to be adjusted
    -Trail length is affected by number of rat followers and slightly extended by player velocity, adding a controllable stretch effect

--- Assets/Scripts/MasterRatController.cs
+++ Assets/Scripts/MasterRatController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Linq;
 
 /// <summary>
 /// Controls the big rat and governs behavior of all the little rats.
@@ -15,16 +16,19 @@ public class MasterRatController : MonoBehaviour
     public static MasterRatController main;
 
     //Objects & Components:
+    private SpriteRenderer sprite; //Sprite renderer component for big rat
+
     [Header("Settings:")]
     [SerializeField, Tooltip("Interchangeable data object describing movement settings of main rat")] private BigRatSettings settings;
     [SerializeField, Tooltip("Interchangeable data object describing rat horde behavior")]            private SwarmSettings swarmSettings;
-    private SpriteRenderer sprite; //Sprite renderer component for big rat
 
     //Runtime Vars:
+    internal List<RatBoid> followerRats = new List<RatBoid>(); //List of all rats currently following this controller
+    private List<Vector2> trail = new List<Vector2>();         //List of points in current trail (used to assemble ratswarm behind main rat)
+    private float totalTrailLength = 0;                        //Current length of trail
+
     private Vector2 velocity;  //Current speed and direction of movement
     private Vector2 moveInput; //Current input direction for movement
-
-    private List<Vector2> trailPoints = new List<Vector2>(); //List of points in follower trail behind the big rat (lower indeces are newer)
 
     //RUNTIME METHODS:
     private void Awake()
@@ -33,7 +37,7 @@ public class MasterRatController : MonoBehaviour
         if (settings == null) { Debug.LogError("Big Rat is missing ratSettings object"); Destroy(this); }        //Make sure big rat has ratSettings
         if (swarmSettings == null) { Debug.LogError("Big Rat is missing swarmSettings object"); Destroy(this); } //Make sure big rat has swarmSettings
         if (main == null) main = this; else Destroy(this);                                                       //Singleton-ize this script instance
-        trailPoints.Add(PosAsVector2());                                                                         //Add current position of rat as first trail point
+        trail.Insert(0, PosAsVector2());                                                                         //Add starting position as first point in trail
     }
     private void Start()
     {
@@ -43,6 +47,17 @@ public class MasterRatController : MonoBehaviour
     private void Update()
     {
         MoveRat(Time.deltaTime); //Apply velocity to rat position
+
+        //Visualize trail:
+        if (trail.Count > 1)
+        {
+            for (int i = 1; i < trail.Count; i++)
+            {
+                Vector3 p1 = new Vector3(trail[i].x, 0.1f, trail[i].y);
+                Vector3 p2 = new Vector3(trail[i - 1].x, 0.1f, trail[i - 1].y);
+                Debug.DrawLine(p1, p2, Color.blue);
+            }
+        }
     }
     
     //UPDATE METHODS:
@@ -59,14 +74,19 @@ public class MasterRatController : MonoBehaviour
             velocity += addVel * deltaTime;              //Add velocity as acceleration over time
 
             //Flip sprite:
-            if (moveInput.x != 0) sprite.flipX = moveInput.x > 0 ? true : false; //Flip sprite to direction of horizontal move input
+            if (moveInput.x != 0) sprite.flipX = moveInput.x > 0; //Flip sprite to direction of horizontal move input
         }
         else if (velocity != Vector2.zero) //No input is given but rat is still moving
         {
             velocity = Vector2.MoveTowards(velocity, Vector2.zero, settings.decel * deltaTime); //Slow rat down based on deceleration over time
         }
         else return; //Return if velocity is zero and no input is given
-        velocity = Vector2.ClampMagnitude(velocity, settings.speed); //Clamp velocity to speed
+        float currentSpeed = velocity.magnitude; //Get current speed of main rat
+        if (currentSpeed > settings.speed) //Check if current speed is faster than target speed
+        {
+            velocity = Vector2.ClampMagnitude(velocity, settings.speed); //Clamp velocity to target speed
+            currentSpeed = settings.speed;                               //Update current speed value
+        }
 
         //Get new position:
         Vector3 newPos = transform.position; //Get current position of rat
@@ -75,13 +95,73 @@ public class MasterRatController : MonoBehaviour
         transform.position = newPos;         //Apply new position
 
         //Update trail characteristics:
-        
+        trail.Insert(0, PosAsVector2());                        //Add new trailPoint for current position
+        float segLength = Vector2.Distance(trail[0], trail[1]); //Get length of new segment being created
+        totalTrailLength += segLength;                          //Add length of new segment to total length of trail
+        if (trail.Count > 2) //Only perform culling operations if trail is long enough
+        {
+            //Grow first segment to minimum length:
+            float secondSegLength = Vector2.Distance(trail[1], trail[2]); //Get length of second segment in trail
+            if (secondSegLength < settings.minTrailSegLength) //Check if second segment in trail is too short (first segment can be any length)
+            {
+                totalTrailLength -= segLength + secondSegLength;  //Subtract lengths of both removed segments from total
+                trail.RemoveAt(1);                                //Remove second segment from trail
+                segLength = Vector2.Distance(trail[0], trail[1]); //Get new length of first segment
+                totalTrailLength += segLength;                    //Add length of new segment back to total
+            }
+
+            //Limit trail length:
+            //float targetTrailLength = followerRats.Count * settings.trailLengthPerRat; //Get target trail length based off of follower count and settings
+            float targetTrailLength = 20 * settings.trailLengthPerRat; //Get target trail length based off of follower count and settings
+            targetTrailLength *= Mathf.Lerp(1, settings.velTrailLengthMultiplier, currentSpeed / settings.speed); //Apply velocity-based length multiplier to target trail length
+            while (totalTrailLength > targetTrailLength) //Current trail is longer than target length
+            {
+                float extraLength = totalTrailLength - targetTrailLength;     //Get amount of extra length left in trail
+                float lastSegLength = Vector2.Distance(trail[^1], trail[^2]); //Get distance between last two segments in trail
+                if (extraLength >= lastSegLength) //Last segment is shorter than length which needs to be removed
+                {
+                    trail.RemoveAt(trail.Count - 1);   //Remove last segment from trail
+                    totalTrailLength -= lastSegLength; //Subtract length of removed segment from total
+                }
+                else //Last segment is longer than length which needs to be removed
+                {
+                    trail[^1] = Vector2.MoveTowards(trail[^1], trail[^2], extraLength); //Shorten last segment by extra length
+                    totalTrailLength -= extraLength;                                    //Subtract remaining extra length from total
+                }
+            }
+
+            //Check for crossed lines:
+
+        }
     }
 
     //INPUT METHODS:
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>(); //Store input value
+    }
+
+    //FUNCTIONALITY METHODS:
+    /*/// <summary>
+    /// Returns the point on trail which is closest to given reference point.
+    /// </summary>
+    public Vector2 GetClosestPointOnTrail(Vector2 reference)
+    {
+
+    }*/
+    /// <summary>
+    /// Spawns a new rat and adds it to the swarm.
+    /// </summary>
+    public void AddRat()
+    {
+
+    }
+    /// <summary>
+    /// Removes a specific rat from the swarm.
+    /// </summary>
+    public void RemoveRat()
+    {
+
     }
 
     //UTILITY METHODS:
--- Assets/Scripts/Namespaces.cs
+++ Assets/Scripts/Namespaces.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace CustomEnums
 {
     /// <summary>
